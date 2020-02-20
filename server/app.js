f7aff0a819f5b92246c926bf57fe9b0b4b666373
Sukaato <sukaato.dev@gmail.com>
2020-02-20 22:44:33 +0100

    feat: add pug dependency

--- server/app.js
+++ server/app.js
@@ -7,12 +7,18 @@ import { indexRouter } from './routes/index';
 import { usersRouter } from './routes/users';
 
 const app = express();
+const directories = {
+  views: "../public/views/",
+  public: "../public/"
+};
 
+app.set('views', path.join(__dirname, directories.views));
+app.set('view engine', 'pug');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static(path.join(__dirname, directories.public)));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
