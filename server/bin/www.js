8f9ad7e47e2c727e48750f4f2f86c683cb53fa12
Sukaato <sukaato.dev@gmail.com>
2020-02-20 22:45:54 +0100

    feat: change app name

--- server/bin/www.js
+++ server/bin/www.js
@@ -8,12 +8,12 @@ import debugLib from 'debug';
 import http from 'http';
 
 
-const debug = debugLib('express-es6-sample:server');
+const debug = debugLib('lib-express:server');
 
 /**
  * Get port from environment and store in Express.
  */
-const port = normalizePort(process.env.PORT || '3000');
+const port = normalizePort(process.env.PORT || '3333');
 app.set('port', port);
 
 /**
@@ -24,7 +24,6 @@ const server = http.createServer(app);
 /**
  * Listen on provided port, on all network interfaces.
  */
-
 server.listen(port);
 server.on('error', onError);
 server.on('listening', onListening);
@@ -35,10 +34,10 @@ server.on('listening', onListening);
 function normalizePort(val) {
   const port = parseInt(val, 10);
 
-  // named pipe
+  /* named pipe */
   if (isNaN(port)) return val;
 
-  // port number
+  /* port number */
   if (port >= 0) return port;
 
   return false;
@@ -56,19 +55,21 @@ function onError(error) {
     ? `Pipe ${port}`
     : `Port ${port}`;
 
-  // handle specific listen errors with friendly messages
+  /* handle specific listen errors with friendly messages */
   switch (error.code) {
     case 'EACCES':
       console.error(bind + ' requires elevated privileges');
+      server.close();
       process.exit(1);
       break;
     case 'EADDRINUSE':
       console.error(bind + ' is already in use');
+      server.close();
       process.exit(1);
       break;
     default:
       throw error;
-  }
+      }
 }
 
 /**
