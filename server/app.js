3b97ae34884ee6533a5cc2c6985d938c5593668c
Sukaato <sukaato.dev@gmail.com>
2020-02-25 22:59:02 +0100

    fix: Resolve listen port error

--- server/app.js
+++ server/app.js
@@ -2,25 +2,27 @@ import express from 'express';
 import path from 'path';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
+import createError from 'http-errors';
 
 import { lib_express } from './database/connection/dbConnection';
 import { indexRouter } from './routes/index';
 import { usersRouter } from './routes/users';
 
 const app = express();
-const directories = {
-  views: "../public/views/",
-  public: "../public/"
+const PORT = process.env.NODE_PORT || 3000;
+const dirs = {
+  views: '../public/views/',
+  public: 'public'
 };
 
-app.set('views', path.join(__dirname, directories.views));
+app.set('views', path.join(__dirname, dirs.views));
 app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, directories.public)));
+app.use(express.static(path.join(__dirname, dirs.public)));
 
 
 app.use('/', indexRouter);
@@ -28,8 +30,6 @@ app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
-  res.status(404);
-
   next(createError(404));
 });
 
@@ -44,16 +44,19 @@ app.use((err, req, res, next) => {
   
   // render the error page
   res.status(err.status);
-  res.render(err.status.toString());
+  res.render({title: "SERVER ERROR", error: {status: err.status.toString()}});
 });
 
 lib_express.connect(err => {
   if (err) {
-    throw `[DATABASE] [ERROR] ${err.sqlMessage}`;
+    console.log(`[ DATABASE ] [ ERROR ] ${err.sqlMessage}`);
+    throw `[ APP      ] [       ] L'application ne peux se lancer car la base de bonnée n'est pas accésible`;
+  } else {
+    app.listen(PORT, (err) => {
+      if (err) console.error('Unable to connect the server: ', err);
+      console.log(`Server listening on port ${PORT}`);
+    });
   }
-  app.listen(3333, () => {
-    console.log('Server listening...');
-  });
 });
 
 export default app;
